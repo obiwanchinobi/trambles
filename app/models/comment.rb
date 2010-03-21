ab8a4279cc5ecbc3d908e5aecf248d79967ca621
David Tong <david.tong@digiaid.com.au>
2010-03-21 16:48:13 +1100

    Model validations
    * Rename landing page title
    * Add model validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -20,6 +20,14 @@ class Comment < ActiveRecord::Base
   end
   
   def ramble_fields
-    
+    if category == BUS
+      errors.add_to_base("Bus Company required") if transport.blank?
+      errors.add_to_base("Bus Number required") if vehicle_no.blank?
+    elsif category == FERRY
+      errors.add(:ferry_line, "required") if ferry_line.blank?
+    elsif category == TRAIN
+      errors.add(:train_line, "required") if train_line.blank?
+      errors.add(:direction, "required") if train_line.present? && direction.blank?
+    end
   end
 end
